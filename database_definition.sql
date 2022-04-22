9f2e5d6594179d5394026e48cab2c87a4f8e8104
emiledebadts <emiledebadts1@gmail.com>
2022-04-22 14:48:51 +0200

    test database backup

--- database_definition.sql
+++ database_definition.sql
@@ -1,3 +1,107 @@
+CREATE SCHEMA gmw;
+CREATE SCHEMA gld;
+CREATE SCHEMA aanlevering;
+
+-- gmw.delivered_locations definition
+
+-- Drop table
+
+-- DROP TABLE gmw.delivered_locations;
+
+CREATE TABLE gmw.delivered_locations (
+	location_id int4 NOT NULL,
+	registration_object_id int4 NULL,
+	referencesystem varchar NULL,
+	horizontal_positioning_method varchar NULL,
+	CONSTRAINT delivered_locations_pkey PRIMARY KEY (location_id)
+);
+
+
+-- gmw.delivered_vertical_positions definition
+
+-- Drop table
+
+-- DROP TABLE gmw.delivered_vertical_positions;
+
+CREATE TABLE gmw.delivered_vertical_positions (
+	id serial NOT NULL,
+	delivered_vertical_positions_id int4 NULL,
+	registration_object_id int4 NULL,
+	local_vertical_reference_point varchar NULL,
+	"offset" float8 NULL,
+	vertical_datum varchar NULL,
+	ground_level_position float8 NULL,
+	ground_level_positioning_method varchar NULL,
+	CONSTRAINT delivered_vertical_positions_pkey PRIMARY KEY (id)
+);
+
+
+-- gmw.groundwater_monitoring_tubes definition
+
+-- Drop table
+
+-- DROP TABLE gmw.groundwater_monitoring_tubes;
+
+CREATE TABLE gmw.groundwater_monitoring_tubes (
+	id serial NOT NULL,
+	groundwater_monitoring_tube_id int4 NULL,
+	registration_object_id int4 NULL,
+	tube_number int4 NULL,
+	tube_type varchar NULL,
+	artesian_well_cap_present varchar NULL,
+	sediment_sump_present varchar NULL,
+	number_of_geo_ohm_cables int4 NULL,
+	tube_top_diameter int4 NULL,
+	variable_diameter varchar NULL,
+	tube_status varchar NULL,
+	tube_top_position float8 NULL,
+	tube_top_positioning_method varchar NULL,
+	tube_packing_material varchar NULL,
+	tube_material varchar NULL,
+	glue varchar NULL,
+	screen_length float8 NULL,
+	sock_material varchar NULL,
+	plain_tube_part_length float8 NULL,
+	sediment_sump_length float8 NULL,
+	deliver_to_bro bool NULL,
+	CONSTRAINT groundwater_monitoring_tubes_pkey PRIMARY KEY (id)
+);
+
+
+-- gmw.groundwater_monitoring_wells definition
+
+-- Drop table
+
+-- DROP TABLE gmw.groundwater_monitoring_wells;
+
+CREATE TABLE gmw.groundwater_monitoring_wells (
+	id serial NOT NULL,
+	registration_object_id int4 NULL,
+	registration_object_type varchar NULL,
+	bro_id varchar(15) NULL,
+	request_reference varchar(255) NULL,
+	delivery_accountable_party int4 NULL,
+	delivery_responsible_party int4 NULL,
+	quality_regime varchar NULL,
+	under_privilege varchar NULL,
+	delivery_context varchar NULL,
+	construction_standard varchar NULL,
+	initial_function varchar NULL,
+	number_of_standpipes int4 NULL,
+	ground_level_stable varchar NULL,
+	well_stability varchar NULL,
+	nitg_code varchar(8) NULL,
+	olga_code varchar(8) NULL,
+	well_code varchar NULL,
+	"owner" int4 NULL,
+	maintenance_responsible_party int4 NULL,
+	well_head_protector varchar NULL,
+	well_construction_date date NULL,
+	CONSTRAINT groundwater_monitoring_wells_pkey PRIMARY KEY (id)
+);
+
+
+
 -- gld.groundwater_level_dossier definition
 
 -- Drop table
@@ -290,4 +394,55 @@ CREATE TABLE gld.type_status_quality_control (
 	imbro bool NULL,
 	imbro_a bool NULL,
 	CONSTRAINT type_status_quality_control_pkey PRIMARY KEY (id)
+);
+
+
+-- aanlevering.gld_addition_log definition
+
+-- Drop table
+
+-- DROP TABLE aanlevering.gld_addition_log;
+
+CREATE TABLE aanlevering.gld_addition_log (
+	id bigserial NOT NULL,
+	date_modified varchar(254) NULL,
+	observation_id varchar(254) NULL,
+	"start" varchar(254) NULL,
+	"end" varchar(254) NULL,
+	broid_registration varchar(254) NULL,
+	procedure_uuid varchar(254) NULL,
+	procedure_initialized varchar(254) NULL,
+	validation_status varchar(254) NULL,
+	levering_id varchar(254) NULL,
+	levering_status varchar(254) NULL,
+	"comments" varchar(50000) NULL,
+	file varchar(254) NULL,
+	addition_type varchar(254) NULL,
+	CONSTRAINT gld_addition_log_voorlopig_pkey PRIMARY KEY (id)
+);
+
+
+-- aanlevering.gld_registration_log definition
+
+-- Drop table
+
+-- DROP TABLE aanlevering.gld_registration_log;
+
+CREATE TABLE aanlevering.gld_registration_log (
+	id serial NOT NULL,
+	date_modified varchar(254) NULL,
+	gwm_bro_id varchar(254) NULL,
+	filter_id varchar(254) NULL,
+	validation_status varchar(254) NULL,
+	levering_id varchar(254) NULL,
+	levering_status varchar(254) NULL,
+	gld_bro_id varchar(254) NULL,
+	"comments" varchar(10000) NULL,
+	last_changed varchar(254) NULL,
+	corrections_applied bool NULL,
+	file varchar(254) NULL,
+	quality_regime varchar(254) NULL,
+	timestamp_end_registration timestamptz NULL,
+	process_status varchar(254) NULL,
+	CONSTRAINT gld_registration_log_pkey PRIMARY KEY (id)
 );
