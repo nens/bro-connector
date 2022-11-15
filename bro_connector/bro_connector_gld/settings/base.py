9732702ff1c7f4a50fe9e6847384b3860b6e04d2
KarlSchutt <56637353+KarlSchutt@users.noreply.github.com>
2022-11-15 16:13:59 +0100

    bro_connector

--- bro_connector/bro_connector_gld/settings/base.py
+++ bro_connector/bro_connector_gld/settings/base.py
@@ -89,10 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'grondwatermeetnet',                      
-        'USER': 'bro',
-        'PASSWORD': '@BRO-2022x!',
-        'HOST': 'geoint.zeeland.nl',
-        'PORT': '5435',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
         'OPTIONS':{'options': '-c search_path=django_admin'}
 }}
 
