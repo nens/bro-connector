c25a2b1de153f635d178d398b3b9a5b2df125ae4
KarlSchutt <56637353+KarlSchutt@users.noreply.github.com>
2022-11-15 17:49:45 +0100

    remove cmd

--- bro_connector/django.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Activate VENV
-call C:\Users\karl.schutt\Documents\venvs\bro-zeeland\Scripts\activate.bat
-call C:\Users\karl.schutt\Documents\venvs\bro-zeeland\Scripts\python.exe manage.py %*
