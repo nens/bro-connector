33e419750ab10701cc03ae90d59ccf3409eb6c46
StevenHosper <steven.hosper@ziggo.nl>
2023-11-22 11:36:54 +0100

    Uitbreiding database

---
+++ bro_connector/gmw/choices.py
@@ -0,0 +1,31 @@
+WELLHEADPROTECTOR_SUBTYPES = (
+    ("DNS", "DNS"),
+    ("model X", "model X"),
+    ("onbekend", "onbekend"),
+)
+
+LOCKS = (
+    ("sleutel", "sleutel"),
+    ("inbus", "inbus")
+    ("onbekend", "onbekend"),
+)
+
+LABELS = (
+    ("geen", "geen"),
+    ("sticker", "sticker"),
+    ("aluplaatje", "aluplaatje")
+    ("onbekend", "onbekend"),
+)
+
+FOUNDATIONS = (
+    ("geen", "geen"),
+    ("betonomstorting", "betonomstorting"),
+    ("onbekend", "onbekend"),
+)
+
+COLLISION_PROTECTION_TYPES = (
+    ("geen", "geen"),
+    ("palen", "palen"),
+    ("hekwerk", "hekwerk")
+    ("onbekend", "onbekend"),
+)
