11297b468ab96bc94031f2432babca28064093c6
StevenHosper <steven.hosper@ziggo.nl>
2023-11-24 16:58:29 +0100

    split bro_uitgifte into handlers

---
+++ bro_connector/main/management/tasks/bro_handlers.py
@@ -0,0 +1,252 @@
+import requests
+import requests.auth
+from abc import ABC, abstractmethod
+import xml.etree.ElementTree as ET
+from icecream import *
+
+DB_NAME = "grondwatermeetnet"
+DB_USER = "postgres"
+DB_PASS = "!StNS!2023"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+
+# FORMULAS USED IN HISTORIE_OPHALEN COMMAND DJANGO ZEELAND
+def slice(sourcedict, string):
+    newdict = {}
+    for key in sourcedict.keys():
+        if key.startswith(string):
+            newdict[key.split(string)[1]] = sourcedict[key]
+    return newdict
+
+class BROHandler(ABC):
+    @abstractmethod
+    def get_data(self, id: str, full_history: bool) -> None:
+        pass
+
+    @abstractmethod
+    def root_data_to_dictionary(self):
+        pass
+    
+    @abstractmethod
+    def reset_values(self):
+        pass
+
+class GMWHandler(BROHandler):
+    def __init__(self):
+        self.number_of_events = 0
+        self.number_of_tubes  = 0
+        self.number_of_geo_ohm_cables = 0
+        self.number_of_electrodes = 0
+        self.positions = 0
+        self.dict = {}
+
+    def get_data(self, id: str, full_history: bool):
+        basis_url = "https://publiek.broservices.nl/gm/gmw/v1/objects/"
+
+        if full_history:
+            fh = "ja"
+        else:
+            fh = "nee"
+
+        gmw_verzoek = requests.get("{}{}?fullHistory={}".format(basis_url, id, fh))
+
+        self.root = ET.fromstring(gmw_verzoek.content)
+
+
+    def root_data_to_dictionary(self):
+        tags = []
+        values = []
+        prefix = ""
+        number_of_events = self.number_of_events
+
+        for element in self.root.iter():
+            tag = element.tag
+            split = tag.split("}")
+
+            if split[1] == "wellConstructionDate":
+                prefix = "construction_"
+
+            if split[1] == "intermediateEvent":
+                number_of_events = number_of_events + 1
+                prefix = "event_" + str(number_of_events) + "_"
+
+            if split[1] == "wellRemovalDate":
+                prefix = "removal_"
+
+            if split[1] == "monitoringTube":
+                self.number_of_tubes = self.number_of_tubes + 1
+                prefix = "tube_" + str(self.number_of_tubes) + "_"
+
+            if split[1] == "geoOhmCable":
+                self.number_of_geo_ohm_cables = self.number_of_geo_ohm_cables + 1
+                prefix = f"tube_{self.number_of_tubes}_geo_ohm_{str(self.number_of_geo_ohm_cables)}_"
+
+            if split[1] == "electrode":
+                self.number_of_electrodes = self.number_of_electrodes + 1
+                prefix = f"tube_{self.number_of_tubes}_geo_ohm_{str(self.number_of_geo_ohm_cables)}_electrode_{str(self.number_of_electrodes)}_"
+            
+            tag = str(prefix) + split[1]
+            
+            if split[1] == "pos":
+                self.positions = self.positions + 1
+                postfix = f"_{self.positions}"
+                tag = split[1] + postfix
+
+            tags.append(tag)
+            values.append(element.text)
+        
+        self.number_of_events = number_of_events
+
+        self.dict = dict(zip(tags, values))
+
+    def reset_values(self):
+        self.number_of_events = 0
+        self.number_of_tubes  = 0
+        self.number_of_geo_ohm_cables = 0
+        self.number_of_electrodes = 0
+        self.positions = 0
+
+class GLDHandler(BROHandler):
+    def __init__(self):
+        self.dict = {}
+
+    def get_data(self, id: str, full_history: bool):
+        basis_url = "https://publiek.broservices.nl/gm/gld/v1/objects/"
+
+        if full_history:
+            fh = "ja"
+        else:
+            fh = "nee"
+
+        gmw_verzoek = requests.get("{}{}?fullHistory={}".format(basis_url, id, fh))
+
+        self.root = ET.fromstring(gmw_verzoek.content)
+
+
+    def root_data_to_dictionary(self):
+        tags = []
+        values = []
+        prefix = ""
+
+        for element in self.root.iter():
+            tag = element.tag
+            split = tag.split("}")
+            
+            tag = str(prefix) + split[1]
+
+            tags.append(tag)
+            values.append(element.text)
+
+        self.dict = dict(zip(tags, values))
+
+    def reset_values(self):
+        pass
+
+class GMNHandler(BROHandler):
+    def __init__(self):
+        self.dict = {}
+
+    def get_data(self, id: str, full_history: bool):
+        basis_url = "https://publiek.broservices.nl/gm/gmn/v1/objects/"
+
+        if full_history:
+            fh = "ja"
+        else:
+            fh = "nee"
+
+        gmw_verzoek = requests.get("{}{}?fullHistory={}".format(basis_url, id, fh))
+
+        self.root = ET.fromstring(gmw_verzoek.content)
+
+
+    def root_data_to_dictionary(self):
+        tags = []
+        values = []
+        prefix = ""
+
+        for element in self.root.iter():
+            tag = element.tag
+            split = tag.split("}")
+            
+            tag = str(prefix) + split[1]
+
+            tags.append(tag)
+            values.append(element.text)
+
+        self.dict = dict(zip(tags, values))
+
+    def reset_values(self):
+        pass
+
+class GARHandler(BROHandler):
+    def __init__(self):
+        self.dict = {}
+
+    def get_data(self, id: str, full_history: bool):
+        basis_url = "https://publiek.broservices.nl/gm/gar/v1/objects/"
+
+        if full_history:
+            fh = "ja"
+        else:
+            fh = "nee"
+
+        gmw_verzoek = requests.get("{}{}?fullHistory={}".format(basis_url, id, fh))
+
+        self.root = ET.fromstring(gmw_verzoek.content)
+
+
+    def root_data_to_dictionary(self):
+        tags = []
+        values = []
+        prefix = ""
+
+        for element in self.root.iter():
+            tag = element.tag
+            split = tag.split("}")
+            
+            tag = str(prefix) + split[1]
+
+            tags.append(tag)
+            values.append(element.text)
+
+        self.dict = dict(zip(tags, values))
+
+    def reset_values(self):
+        pass
+
+class FRDHandler(BROHandler):
+    def __init__(self):
+        self.dict = {}
+
+    def get_data(self, id: str, full_history: bool):
+        basis_url = "https://publiek.broservices.nl/gm/frd/v1/objects/"
+
+        if full_history:
+            fh = "ja"
+        else:
+            fh = "nee"
+
+        gmw_verzoek = requests.get("{}{}?fullHistory={}".format(basis_url, id, fh))
+
+        self.root = ET.fromstring(gmw_verzoek.content)
+
+
+    def root_data_to_dictionary(self):
+        tags = []
+        values = []
+        prefix = ""
+
+        for element in self.root.iter():
+            tag = element.tag
+            split = tag.split("}")
+            
+            tag = str(prefix) + split[1]
+
+            tags.append(tag)
+            values.append(element.text)
+
+        self.dict = dict(zip(tags, values))
+
+    def reset_values(self):
+        pass
---
+++ bro_connector/main/management/tasks/kvk_handler.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+class DataRetrieverKVK:
+    def __init__(self, kvk_nummer):
+        self.kvk_nummer = kvk_nummer
+        self.bro_ids = []
+
+    def request_bro_ids(self, type):
+        options = ['gmw', 'frd', 'gar', 'gmn', 'gld']
+        if type.lower() not in options:
+            raise Exception(f"Unknown type: {type}. Use a correct option: {options}.")
+
+        basis_url = "https://publiek.broservices.nl"
+        kvk_verzoek = requests.get(
+             f"{basis_url}/gm/{type}/v1/bro-ids?bronhouder={str(self.kvk_nummer)}"
+        )
+        self.bro_ids = json.loads(kvk_verzoek.text)["broIds"]
+
+    def get_ids_kvk(self):
+        self.gmw_ids = []
+        self.gld_ids = []
+        self.frd_ids = []
+        self.gar_ids = []
+        self.gmn_ids = []
+        self.other_ids = []
+
+        for id in self.bro_ids:
+            if id.startswith("GMW"):
+                self.gmw_ids.append(id)
+
+            elif id.startswith("GLD"):
+                self.gld_ids.append(id)
+
+            elif id.startswith("FRD"):
+                self.frd_ids.append(id)
+
+            elif id.startswith("GAR"):
+                self.gar_ids.append(id)
+
+            elif id.startswith("GMN"):
+                self.gmn_ids.append(id)
+
+            else:
+                self.other_ids.append(id)
