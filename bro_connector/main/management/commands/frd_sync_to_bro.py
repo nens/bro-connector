7c097d976467d180ca945c5b6b4997b803a73eb0
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2023-12-29 11:29:03 +0100

    reorganisation code

--- bro_connector/main/management/commands/frd_sync_to_bro.py
+++ bro_connector/main/management/commands/frd_sync_to_bro.py
@@ -5,19 +5,46 @@ from bro_exchange.broxml.frd.requests import FrdStartregistrationTool
 from frd.models import FormationResistanceDossier, FrdSyncLog
 from datetime import datetime
 
+#############
+## Command ##
+#############
 
 class Command(BaseCommand):
     """
     Handles the sync between the bro-connector and the BRO.
     Runs daily as cronjob.
     """
-
     help = "Syncs the FRD data to the BRO"
 
     def handle(self, *args, **kwargs):
-        sync = FrdSync()
-        sync.run()
+        self.handle_startregistrations()
+        self.handle_measurements()
 
+    def handle_startregistrations(self):
+        """
+        Handles the startregistrations for all FRD's without bro_id.
+        For each new FRD, the FrdStartregistration class is used to hande the registration
+        """
+        new_dossiers_qs = self.get_new_dossiers()
+
+        if len(new_dossiers_qs) != 0:
+            for new_dossier in new_dossiers_qs:
+                startregistration = FrdStartregistration(new_dossier)
+                startregistration.sync()
+
+    def get_new_dossiers(self):
+        return FormationResistanceDossier.objects.filter(frd_bro_id=None)
+    
+    def handle_measurements(self):
+        """
+        Develop when startregistrations are done
+        """
+        pass
+
+
+##############################
+## General helper functions ##
+##############################
 
 def update_log_obj(log_obj, field_value_dict):
     """
@@ -31,35 +58,15 @@ def update_log_obj(log_obj, field_value_dict):
     
     log_obj.save()
 
-    
-class FrdSync:
-    """
-    Syncs all data from the BRO-Connector to the BRO.
-    Loops over the FRDs in the database to check if startregistrations are required,
-    And checks for new measurement values.
-    If new data is found, the BRO-Exchange tool is used to create, validate, and send the XML files.
-    """
 
-    def run(self):
-        """
-        Handle startregistrations
-        """
-        new_dossiers_qs = self.get_new_dossiers()
-        if len(new_dossiers_qs) != 0:
-            for new_dossier in new_dossiers_qs:
-                startregistration = FrdStartregistration(new_dossier)
-                startregistration.sync()
-
-
-
-    def get_new_dossiers(self):
-        return FormationResistanceDossier.objects.filter(frd_bro_id=None)
-
+#############
+## Classes ##
+#############
 
 class FrdStartregistration:
     """
-    Handles the startregistration of a new FRD in the system,
-    based on the qs obj_id.
+    Handles the startregistration of a new FRD in the BRO-Connector.
+    The sync method is the main function which should always be called
     """
 
     def __init__(self, frd_obj):
@@ -69,31 +76,65 @@ class FrdStartregistration:
         self.frd_startregistration_log = None
 
     def sync(self):
+        """
+        Checks if a log on the Dossier allready exists. 
+        If not, creates one and handles the complete startregistrations.
+        If so, checks the status, and picks up the startregistration process where it was left.
+        """
         self.frd_startregistration_log, created = FrdSyncLog.objects.update_or_create(
             event_type="FRD_StartRegistration", frd=self.frd_obj
         )
 
-        if self.frd_startregistration_log.process_status in [
-            None,
-            "failed_to_generate_startregistration_xml",
-        ]:
-            try:
-                self.test()
-                self.generate_startregistration_xml()
-                self.save_xml_file()
-                update_log_obj(
-                    self.frd_startregistration_log,
-                    {"process_status": "succesfully_generated_startregistration_xml"},
-                )
+        status_function_mapping = {
+            None:self.generate_xml_file,
+            "failed_to_generate_startregistration_xml":self.generate_xml_file,
+            "succesfully_generated_startregistration_xml":self.validate_startregistration_xml,
+        }
 
-            except Exception as e:
-                update_log_obj(
-                    self.frd_startregistration_log,
-                    {"process_status": "failed_to_generate_startregistration_xml",
-                     "comment":f"Error message: {e}"},
-                )
+        current_status = self.frd_startregistration_log.process_status
+        method_to_call = status_function_mapping.get(current_status)
 
-    def generate_startregistration_xml(self):
+        method_to_call()
+
+            
+    def generate_xml_file(self):
+        """
+        Handles the generation of the startregistration xlm file.
+        If it is succesfull, it calls the validate_startregistration_xml to continue the startregistration process.
+        """
+        try:
+            self.construct_xml_tree()
+            self.save_xml_file()
+            update_log_obj(
+                self.frd_startregistration_log,
+                {"process_status": "succesfully_generated_startregistration_xml",
+                    "comment":"Succesfully generated startregistration request"},
+            )
+
+        except Exception as e:
+            update_log_obj(
+                self.frd_startregistration_log,
+                {"process_status": "failed_to_generate_startregistration_xml",
+                    "comment":f"Error message: {e}"},
+            )
+        
+        else:
+            self.validate_startregistration_xml()
+
+    def validate_startregistration_xml(self):
+        print('validating')
+
+
+
+
+
+
+
+
+
+
+
+    def construct_xml_tree(self):
         """
         Setup the data for the startregistration xml file.
         Then creates the file and saves it in the assigned folder.
