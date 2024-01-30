36ce884e3f63c54b0e279343d942d54a485413f6
StevenHosper <steven.hosper@ziggo.nl>
2024-01-30 16:46:49 +0100

    Scripts to move and assign electrode numbers

---
+++ bro_connector/main/management/commands/assign_electrode_numbers.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from gmw.models import (
+    ElectrodeStatic,
+)
+import reversion
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        electrodes = ElectrodeStatic.objects.filter(
+            electrode_number = None,
+        )
+        
+        unique_cables = []
+
+        for electrode in electrodes:
+            print(electrode)
+            if electrode.geo_ohm_cable in unique_cables:
+                # Has already been handled.
+                continue
+
+            electrodes_of_cable_ordered = ElectrodeStatic.objects.filter(
+                geo_ohm_cable = electrode.geo_ohm_cable
+            ).order_by("electrode_position")
+
+            num = 1
+            for orderded_electrode in electrodes_of_cable_ordered:
+                print(orderded_electrode)
+                with reversion.create_revision():
+                    orderded_electrode.electrode_number = num
+                    orderded_electrode.save()
+                    reversion.set_comment("Used a script to assign an electrode number as none was known.")
+                
+                num += 1
+            
+            unique_cables.append(electrode.geo_ohm_cable)
