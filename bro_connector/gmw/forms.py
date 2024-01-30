92da2f264640cce319d05c1ef6b36d0e017e7296
StevenHosper <steven.hosper@ziggo.nl>
2024-01-30 16:46:17 +0100

    Accept picture upload

--- bro_connector/gmw/forms.py
+++ bro_connector/gmw/forms.py
@@ -1,6 +1,29 @@
 import django.forms as forms
 from . import models
 
+class BinaryFileInput(forms.ClearableFileInput):
+
+    def is_initial(self, value):
+        """
+        Return whether value is considered to be initial value.
+        """
+        return bool(value)
+
+    def format_value(self, value):
+        """Format the size of the value in the db.
+
+        We can't render it's name or url, but we'd like to give some information
+        as to wether this file is not empty/corrupt.
+        """
+        if self.is_initial(value):
+            return f'{len(value)} bytes'
+
+
+    def value_from_datadict(self, data, files, name):
+        """Return the file contents so they can be put in the db."""
+        upload = super().value_from_datadict(data, files, name)
+        if upload:
+            return upload.read()
 
 class GroundwaterMonitoringWellStaticForm(forms.ModelForm):
     x = forms.CharField(required=True)
