bd063d25c085a5c7ac1164af7edc4d865a1501c2
StevenHosper <steven.hosper@ziggo.nl>
2024-04-04 09:19:34 +0200

    app view

--- bro_connector/qc_tool/views.py
+++ bro_connector/qc_tool/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.shortcuts import render
-import qc_tool.test_app
+from qc_tool import test_app
+from django.contrib import admin
+
+# Some initialisation/discovery stuff
+admin.autodiscover()
+test_app
 
 def render_qc_tool(request):
     return render(request, "dash.html")
