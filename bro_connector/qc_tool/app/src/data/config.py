23335b3f3674305693d6c50957763564095176db
StevenHosper <steven.hosper@ziggo.nl>
2024-04-04 10:33:26 +0200

    localsettings

--- bro_connector/qc_tool/app/src/data/config.py
+++ bro_connector/qc_tool/app/src/data/config.py
@@ -1,6 +1,6 @@
 # This file contains the configuration for the Database
-database = "gmw"
-user = "david"
-password = "admin"
+database = "grondwatermeetnet"
+user = "postgres"
+password = "postgres"
 host = "localhost"
 port = 5432
