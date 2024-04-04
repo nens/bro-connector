65c315d896ea2519cbe179e16a4be2cb69b39f11
StevenHosper <steven.hosper@ziggo.nl>
2024-04-04 09:19:17 +0200

    app for testing

--- bro_connector/qc_tool/test_app.py
+++ bro_connector/qc_tool/test_app.py
@@ -1,83 +1,53 @@
 import logging
 import os
-
 import dash_bootstrap_components as dbc
 import i18n
 import pastastore as pst
-from .callbacks import register_callbacks
-from dash import CeleryManager, Dash, DiskcacheManager
+from dash import CeleryManager, DiskcacheManager
 from django_plotly_dash import DjangoDash
-
-from .src.cache import cache
-from .src.components.layout import create_layout
+import dash
 from .src.data.source import DataInterface,  DataSource, TravalInterface
-
-from django.apps import AppConfig
+from dash import dcc, html
 
 
 
 logger = logging.getLogger("waitress")
 logger.setLevel(logging.ERROR)
 
-# %% set some variables
-external_stylesheets = [
-    dbc.themes.FLATLY,
-    "https://use.fontawesome.com/releases/v6.5.1/css/all.css",
-]
-
-# %% main app
-
-# %% set the locale and load the translations
-LOCALE = "en"
-
-i18n.set("locale", LOCALE)
-i18n.load_path.append("locale")
-
-# %% Set up backend
-
-# connect to the database
-db = DataSource()
-# data = DataSourceHydropandas(fname="obs_collection_dino.pickle")
-
-# load pastastore
-# name = "zeeland"
-name = "zeeland_bro"
-conn = pst.ArcticDBConnector(name=name, uri="lmdb://../../traval_scripts/pastasdb/")
-pstore = pst.PastaStore(conn)
-print(pstore)
-
-# load ruleset
-traval_interface = TravalInterface(db, pstore)
-
-# add all components to our data interface object
-data = DataInterface(db=db, pstore=pstore, traval=traval_interface)
-
-# %%
-
-if "REDIS_URL" in os.environ:
-    # Use Redis & Celery if REDIS_URL set as an env variable
-    from celery import Celery
-
-    celery_app = Celery(
-        __name__,
-        broker=os.environ["REDIS_URL"],
-        backend=os.environ["REDIS_URL"],
-    )
-    background_callback_manager = CeleryManager(celery_app)
-
-else:
-    # Diskcache for non-production apps when developing locally
-    import diskcache
-
-    callback_cache = diskcache.Cache("./.cache")
-    background_callback_manager = DiskcacheManager(callback_cache)
-
 # %% build app
 
-app = DjangoDash('QCTool')
+app = DjangoDash(
+    'QC_Tool',
+    serve_locally=False,
+)
 
-app.title = i18n.t("general.app_title")
-app.layout = create_layout(app, data)
+app.layout = html.Div([
+    dcc.RadioItems(
+        id='dropdown-color',
+        options=[{'label': c, 'value': c.lower()} for c in ['Red', 'Green', 'Blue']],
+        value='red'
+    ),
+    html.Div(id='output-color'),
+    dcc.RadioItems(
+        id='dropdown-size',
+        options=[{'label': i,
+                  'value': j} for i, j in [('L','large'), ('M','medium'), ('S','small')]],
+        value='medium'
+    ),
+    html.Div(id='output-size')
 
-# register callbacks
-register_callbacks(app, data)
+])
+
+@app.callback(
+    dash.dependencies.Output('output-color', 'children'),
+    [dash.dependencies.Input('dropdown-color', 'value')])
+def callback_color(dropdown_value):
+    return "The selected color is %s." % dropdown_value
+
+@app.callback(
+    dash.dependencies.Output('output-size', 'children'),
+    [dash.dependencies.Input('dropdown-color', 'value'),
+     dash.dependencies.Input('dropdown-size', 'value')])
+def callback_size(dropdown_color, dropdown_size):
+    return "The chosen T-shirt is a %s %s one." %(dropdown_size,
+                                                  dropdown_color)
