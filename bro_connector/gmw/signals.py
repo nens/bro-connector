c5d4a966bfee66881a6b2fce259dbb21b7c49495
StevenHosper <steven.hosper@ziggo.nl>
2024-06-11 17:02:49 +0200

    add synchronisatie signal to update bro-id in well

--- bro_connector/gmw/signals.py
+++ bro_connector/gmw/signals.py
@@ -1,1 +1,26 @@
-# Create your signals here.
+from django.db.models.signals import (
+    m2m_changed,
+    post_delete,
+    post_save,
+    pre_delete,
+    pre_save,
+)
+from django.dispatch import receiver
+from .models import gmw_registration_log, Event
+import reversion
+
+@receiver(post_save, sender=gmw_registration_log)
+def on_save_gmw_synchronisatie_log(sender, instance: gmw_registration_log, created, **kwargs):
+    if instance.levering_type != "Construction":
+        return
+    
+    if instance.bro_id is not None:
+        event = Event.objects.get(change_id=instance.event_id)
+        well = event.groundwater_monitoring_well_static
+        if well.bro_id != instance.bro_id:
+            with reversion.create_revision():
+                well.bro_id = instance.bro_id
+                well.save(update_fields=["bro_id"])
+                reversion.set_comment(f"Updated BRO-ID based on sync_log ({instance.id}).")
+
+    
