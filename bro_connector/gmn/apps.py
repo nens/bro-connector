ce6067ca0d88edc25915a7f9a20d5f0fe8172359
StevenHosper <steven.hosper@ziggo.nl>
2024-06-14 15:03:49 +0200

    allow for gmn signals

--- bro_connector/gmn/apps.py
+++ bro_connector/gmn/apps.py
@@ -4,3 +4,6 @@ class GmnAanleveringConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gmn"
     verbose_name = "GMN"
+
+    def ready(self):
+        import gmn.signals
