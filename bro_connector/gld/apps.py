d15c06625aa7fdc6210fbb8f240fdb6a86888948
StevenHosper <steven.hosper@ziggo.nl>
2024-06-17 14:13:54 +0200

    allow gld signals

--- bro_connector/gld/apps.py
+++ bro_connector/gld/apps.py
@@ -5,3 +5,6 @@ class GldAanleveringConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gld"
     verbose_name = "GLD"
+
+    def ready(self) -> None:
+        import gld.signals
