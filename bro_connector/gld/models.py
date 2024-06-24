65b8f5211e8d3d8c9ef1b12d772fd1be9ecb4f1c
StevenHosper <steven.hosper@ziggo.nl>
2024-06-24 11:14:31 +0200

    Use Organisation instead of ResponsibleParty

--- bro_connector/gld/models.py
+++ bro_connector/gld/models.py
@@ -7,6 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from .choices import *
+from bro.models import Organisation
 from gmw.models import GroundwaterMonitoringTubeStatic
 import datetime
 
@@ -208,14 +209,14 @@ class ObservationMetadata(models.Model):
     )
     status = models.CharField(choices=STATUSCODE, max_length=200, blank=True, null=True)
     responsible_party = models.ForeignKey(
-        "ResponsibleParty", on_delete=models.CASCADE, null=True, blank=True
+        Organisation, on_delete=models.SET_NULL, null=True, blank=True
     )
 
     def __str__(self):
         return "{}, {}, {}".format(
             str(self.date_stamp),
             str(self.status),
-            self.responsible_party.organisation_name,
+            self.responsible_party.name,
         )
 
     class Meta:
@@ -316,21 +317,6 @@ class MeasurementPointMetadata(models.Model):
 
     def __str__(self):
         return str(self.measurement_point_metadata_id)
-
-
-class ResponsibleParty(models.Model):
-    responsible_party_id = models.AutoField(primary_key=True)
-    identification = models.IntegerField(blank=True, null=True)
-    organisation_name = models.CharField(max_length=200, blank=True, null=True)
-
-    class Meta:
-        managed = True
-        db_table = 'gld"."responsible_party'
-        verbose_name = "Verantwoordelijke Partij"
-        verbose_name_plural = "Verantwoordelijke Partij"
-
-    def __str__(self):
-        return "{}".format(self.organisation_name)
 
 
 # %% Aanlevering models
