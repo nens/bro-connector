4e0095cef78dde6cc52d1cd4e7bce4db6fa4e08e
StevenHosper <steven.hosper@ziggo.nl>
2024-06-27 13:24:12 +0200

    When MTVP delete, also delete metadata

--- bro_connector/gld/signals.py
+++ bro_connector/gld/signals.py
@@ -1,13 +1,14 @@
 from django.db.models.signals import (
     post_save,
+    post_delete,
 )
 from django.dispatch import receiver
-from .models import gld_registration_log, GroundwaterLevelDossier
+from .models import gld_registration_log, GroundwaterLevelDossier, MeasurementTvp
 from gmw.models import GroundwaterMonitoringTubeStatic
 import reversion
 
 @receiver(post_save, sender=gld_registration_log)
-def on_save_gmw_synchronisatie_log(sender, instance: gld_registration_log, created, **kwargs):
+def on_save_gld_synchronisatie_log(sender, instance: gld_registration_log, created, **kwargs):
     if instance.gld_bro_id is not None:
         tube = GroundwaterMonitoringTubeStatic.objects.get(
             groundwater_monitoring_well_static__bro_id=instance.gwm_bro_id, 
@@ -19,3 +20,8 @@ def on_save_gmw_synchronisatie_log(sender, instance: gld_registration_log, creat
                 gld.gld_bro_id = instance.gld_bro_id
                 gld.save(update_fields=["gld_bro_id"])
                 reversion.set_comment(f"Updated BRO-ID based on sync_log ({instance.id}).")
+
+@receiver(post_delete, sender=MeasurementTvp)
+def on_delete_measurement_tvp(sender, instance: MeasurementTvp, **kwargs):
+    metadata = instance.measurement_point_metadata
+    metadata.delete()
