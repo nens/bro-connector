f551b682f319756e0a0dd1f8a08d2632341907d3
jzitman <jelle.zitman@nelen-schuurmans.nl>
2025-04-16 13:32:57 +0200

    Synced delivery to GLD delivery field

--- bro_connector/gld/signals.py
+++ bro_connector/gld/signals.py
@@ -79,6 +79,13 @@ def on_save_gld_addition_log(sender, instance: gld_addition_log, created, **kwar
             observation.save(update_fields=["observation_id_bro"])
             reversion.set_comment("Updated observation_id based on delivery to BRO.")
 
+    if instance.delivery_status == "DOORGELEVERD":
+        with reversion.create_revision():
+            observation.up_to_date_in_bro = True
+            observation.save(update_fields=["up_to_date_in_bro"])
+            reversion.set_comment("Updated up_to_date_in_bro as delivery was succesful.")
+
+
 
 @receiver(post_delete, sender=MeasurementTvp)
 def on_delete_measurement_tvp(sender, instance: MeasurementTvp, **kwargs):
