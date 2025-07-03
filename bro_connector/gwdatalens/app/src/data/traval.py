a8053967faa8794b1a20d718124d882f1d4f8f4e
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-07-03 11:52:57 +0200

    modify manual check behavior slightly - mark non-signalled obs as "reliable" if incoming status suggests measurement has not been checked.

--- bro_connector/gwdatalens/app/src/data/traval.py
+++ bro_connector/gwdatalens/app/src/data/traval.py
@@ -253,25 +253,34 @@ class TravalInterface:
                 "incoming_status_quality_control"
             ].apply(lambda v: bro_nl_to_en[v])
 
-        # set current status to blank and mark suspect observations unreliable as
-        # suggestion
+        # start qc as blank
         df["status_quality_control"] = ""
+        # set current status to incoming for reliable, unreliable and undecided
+        mask = df["incoming_status_quality_control"].isin(
+            [
+                i18n.t("general.reliable"),
+                i18n.t("general.unreliable"),
+                i18n.t("general.undecided"),
+            ]
+        )
+        df.loc[mask, "status_quality_control"] = df.loc[
+            mask, "incoming_status_quality_control"
+        ]
+        # and mark suspect observations unreliable as suggestion
         df.loc[df["flagged"] == 1, "status_quality_control"] = i18n.t(
             "general.unreliable"
         )
+        # and mark all observations that are not flagged and not already
+        # labeled with reliable/unreliable/undecided as reliable.
+        df.loc[
+            (df["flagged"] == 0) & (df["status_quality_control"] == ""),
+            "status_quality_control",
+        ] = i18n.t("general.reliable")
 
         df["category"] = ""  # for QC Protocol category
 
         # filter out observations that were already checked
         if only_unvalidated:
-            mask = df["incoming_status_quality_control"].isin(
-                [
-                    i18n.t("general.reliable"),
-                    i18n.t("general.unreliable"),
-                    i18n.t("general.undecided"),
-                ]
-            )
-
             df.loc[mask, "flagged"] = -1  # set already checked to (-1)
             df.loc[mask, "comment"] = ""  # set comments already checked to empty
             df.loc[mask, "status_quality_control"] = ""  # set qc check to empty
